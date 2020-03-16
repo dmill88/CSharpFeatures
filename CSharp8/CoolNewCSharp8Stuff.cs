b1fadbedecd813f2f4e5c91d5452338f7d79c5b6
David Milligan <dmill88@hotmail.com>
2020-03-15 22:30:19 -0700

    Made the CSharp8.GenerateSequence a little faster.

--- CSharp8/CoolNewCSharp8Stuff.cs
+++ CSharp8/CoolNewCSharp8Stuff.cs
@@ -30,7 +30,7 @@ namespace CSharp8
         {
             for (int i = 0; i < 20; i++)
             {
-                await Task.Delay(100);
+                await Task.Delay(50);
                 yield return i;
             }
         }
